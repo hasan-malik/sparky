7b910fa48356ab1c652af531bfd645d87c48b628
Hasan Malik <hasanimranmalik245@gmail.com>
2026-01-30 20:02:50 -0500

    Initial Commit

---
+++ Sparky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sparky
+//
+//  Created by Hasan Malik on 2026-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sparky/SparkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkyApp.swift
+//  Sparky
+//
+//  Created by Hasan Malik on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct SparkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
